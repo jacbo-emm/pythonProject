9c8892c00662d5298fb7e4e726f32330110308d3
jacbo-emm <54000341+jacbo-emm@users.noreply.github.com>
2023-07-16 15:04:26 +0800

    Update main.py
    添加一首诗歌

--- main.py
+++ main.py
@@ -14,3 +14,20 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# 长字符串
+print("""月下独酌其一
+
+花间一壶酒，独酌无相亲。
+
+举杯邀明月，对影成三人。
+
+月既不解饮，影徒随我身。
+
+暂伴月将影，行乐须及春。
+
+我歌月徘徊，我舞影零乱。
+
+醒时相交欢，醉后各分散。
+
+永结无情游，相期邈云汉。""")
